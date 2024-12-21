b80a53ab3e653ac0d9c19d32c6909b33120ae998
@trast2801 <trast2801@yandex.ru>
2024-12-22 00:25:31 +0300

    Добавлена функция, считающая кол-во записей созданных автором. по сигналу джанго

---
+++ urban_project/board/signals.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import user_logged_in
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from vote.models import Vote
+
+from .models import Advertisement, Stat
+
+
+@receiver(post_save, sender=Advertisement)
+def update_advertisement_count_add(sender, instance, created, **kwargs):
+    """
+    Сигнал для обновления количества созданных объявлений.
+    """
+    if created:
+        profile, _ = Stat.objects.get_or_create(user=instance.author)
+        profile.advertisement_count += 1
+
+        profile.save()
+
+
+@receiver(post_delete, sender=Advertisement)
+def update_advertisement_count_del(sender, instance, **kwargs):
+    """
+    Сигнал для обновления количества созданных объявлений. C защитой от минусов
+    """
+    profile, _ = Stat.objects.get_or_create(user=instance.author)
+    if profile.advertisement_count > 0:
+        profile.advertisement_count -= 1
+    else:
+        profile.advertisement_count = 0
+    profile.save()
